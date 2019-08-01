c293ba036f4adb6ce8e25605f91301e472665f5c
Ben Rogmans <ben@rogmansmedia.nl>
2019-08-01 17:22:32 +0200

    Next steps

---
+++ json/henry.js
@@ -0,0 +1,161 @@
+module.exports = {
+  "header": {enName:"Pien Geng, Kwan (Henry)",cnName:"å®˜å½¬è€•",mobile:"+6012-511-8426",email:"henry@piengeng.com",address:"718, Kampung Baru Sungai Chua, 43000 Kajang, Selangor, Malaysia",cnNameBase64:""},
+  "objective": "To seek a permanent position where my time, skills and knowledge could add values to your company. Looking to lead/manage a team. I see myself as a solution architect in the future.",
+  "education": ["<b>Graduate Management Admission Test</b>/<a class='textlink' href='http://www.mba.com/global' target='_blank' rel='noopener noreferrer'>GMAT</a> (2010 - 2015), <a class='textlink' href='http://www.mba.com/global/the-gmat-exam/gmat-exam-scoring/your-score-report/what-percentile-rankings-mean.aspx' target='_blank' rel='noopener noreferrer'>Score</a> 520/800.","<b>Certified Associate in Project Management</b>/<a class='textlink' href='https://www.pmi.org/certifications/types/certified-associate-capm'>CAPM</a> (2009 - 2014).","<b>Attended professional trainings</b> includes <i>Project Management Professional, 8D Problem Solving, Analog Circuit Design, Effective Team Building, Applied 7 QC Tools, Controlling Conducted and Radiated EMI by Design, Project Management Fundamentals, Mistake Proofing, Eagle Test Systems, Flex/microFlex Programming Fundamentals &amp; Mixed Signal Programming, FMEA for Team Members.</i> (2006 - 2011).","<b>Bachelor of Engineering (Honours) Electronics majoring in Computer</b>, Multimedia University, Melaka (2005), CGPA 3.46/4.00 (2<sup>nd</sup> Class Upper).","<b>Sijil Pelajaran Malaysia</b>, SMK Dato' Sri Amar DiRaja, Muar, Johor (1999), 6(A1), 2(A2), 3(B3), (S.A.P. 10)."],
+  "computing": [{main:"Full stack development",list:["React/Redux/Express/Preact","JavaScript/Node","Phalcon/Laravel/CodeIgniter","PHP","SASS/CSS/HTML","Image editing"]},{main:"Linux administration",list:["Debian/Ubuntu","Raspberry Pi","Exim/Dovecot","Apache/Nginx","PHP","Perl","git/bash/vi/sed/awk/iptables"]},{main:"Microsoft Office",list:["Excel","Word","PowerPoint","Project","Visio","VBA"]},{main:"Electronics schematic",list:["Zuken's CR5000"]},{main:"Other programming languages",list:["Go","Python","Flutter/Dart","AutoIt"]},{main:"Used these databases",list:["PostgreSQL/plpgsql","MySQL","SQLite","MongoDB"]},{main:"",list:["Internet Research"]}],
+  "languages": ["English (<a class='textlink' href='https://en.wikipedia.org/wiki/Malaysian_University_English_Test' target='_blank' rel='noopener noreferrer'>MUET</a>: Band 5)","ä¸­æ–‡ã€åŽè¯­","Bahasa Malaysia","Bits &amp; pieces of Hokkien &amp; Cantonese"],
+  "extra": ["Willing to travel and relocate abroad (depending on location).","Like to read on open-source technologies, internet of things &amp; Lee Child's novels.","Able to start immediately, if insisted by hiring party, 1 to 2 weeks' notice is preferred.","Sublime Text as primary tool, Visual Studio Code for JavaScript &amp; Flutter.","Interview is free, technical test isn't, due to limited &amp; precious time as you can deduct from my study schedule.","gmail/<a class='textlink' href='https://github.com/piengeng' target='_blank' rel='noopener noreferrer'>github</a> id: piengeng","This is a demo of over-complicated ðŸ˜¢ single page design using Preact &amp; Sass, compare previous at <a href='https://cv-php.piengeng.com/' target='_blank' rel='noopener noreferrer'>cv-php.piengeng.com</a>, source at <a href='https://github.com/piengeng/cvjs' target='_blank' rel='noopener noreferrer'>github</a>, with <a href='https://developers.google.com/web/tools/lighthouse/' target='_blank' rel='noopener noreferrer'>Lighthouse</a>'s average score around 99%","Please do email/WhatsApp me if my number is unreachable, unknown number will sometimes be treated as robocall."],
+  "downloadables": [
+    "<a href='https://files.piengeng.com/hk_portrait_b.jpg' target='_blank' rel='noopener noreferrer'>Headshot</a>, if you insist. ðŸ™‚",
+    "Scanned <a href='https://files.piengeng.com/hkcerts.zip'>certificates</a>.",
+    "CV in '<a href='https://files.piengeng.com/cv_henry_kwan_2019.pdf'>pdf</a>' or '<a href='https://files.piengeng.com/cv_henry_kwan_2019.doc'>doc</a>' format.",
+    "<a href='https://files.piengeng.com/gallup.zip'>CliftonStrengths | Gallup</a> assessment (2018) ðŸ¤¨",
+    "<a href='https://files.piengeng.com/raw_captchas.zip'>Images</a> that I could overfit, if you'd like a challenge."
+],
+  "history": [
+  {
+    "title":"Team Lead & Architect",
+    "companyName":"Silverstreet Malaysia Sdn Bhd",
+    "companyLocation":"Kuala Lumpur",
+    "duration":"September 2019 - present",
+    "details":[
+      "Having a great time at Silverstreet, working on a platform that handles millions of messages everyday.",
+      "I'm responsible for migrating projects from a monolith to an event driven microservice architecture.",
+      "We're doing performance optimisations by decoupling services, utilising event streaming platforms like Nats Streaming and rewriting PHP services to Golang and Rust.",
+      "Also responsible to empower Silverstreet's big data analysts to train their AI's with real-time data and I enable them to deploy their AI trained algorithms to production. For example to reduce spam messages or to find the optimum routes."
+    ]
+  },
+  {
+    "title":"Exploration, Research & Development",
+    "companyName":"n/a",
+    "companyLocation":"Kajang, Selangor",
+    "duration":"Mar 2019 - August 2019",
+    "details":[
+      "Trying to gain better understanding via ebooks, articles or online video tutorial for these technologies: Docker, Kafka, Cassandra, Jenkins, gRPC. All these might not for the faint of heart, but I'll persevere, nonetheless.",
+      "IoT Project with Flutter (Dart) &amp; MQTT, working demo published at <a href='https://play.google.com/store/apps/details?id=com.piengeng.flick' target='_blank' rel='noopener noreferrer'>Google Play Store</a>.",
+      "Implemented Prometheus for personal VPSs and RPis.",
+      "Implemented web livechat, you can reach me via <a href='http://chat.piengeng.com' target='_blank' rel='noopener noreferrer'>livechat</a> too.",
+      "Deeper diving into Kubernetes, Istio &amp; EFK.",
+      "Refactored personal projects from PHP to Go. (oh yeah~ 18% improvement between pthreads vs. goroutine on preprocessing ~11.5 years of KLSE data.",
+      "Learning 'Machine Learning' with numpy, pandas, sklearn and tensorflow, ML is challenging!"
+    ]
+  },
+  {
+    "title":"IT Manager",
+    "companyName":"Hong Leong Bank Bhd.",
+    "companyLocation":"Kuala Lumpur",
+    "duration":"(Nov 2018 - Mar 2019)",
+    "details":[
+      "Reason for joining: curious about bank's SOA architecture.",
+      "Reason for leaving before confirmation: too many red tape and production support.",
+      "Responsible for IBM's DataPower appliance and APIConnect products, administration, maintenance and development.",
+      "Used Docker to start DataPower self-study, due to lack of experts in-house, knowledge sharing on how to do things differently in automated way.",
+      "Wrote script to summarize IBM's IIB raw logs for slowness debug and Wireshark analysis on packet dumps.",
+      "Wrote restart script for APIConnect playpen environment, due to unstable product. Ensure connectivity for cluster setup.",
+      "Participate in Data Recovery Plan and Maintenance (scheduled shutdown) activities.",
+      "Typical red tape (meetings, follow-ups, forms, procedures and policies, etc)."
+    ]
+  },
+  {
+    "title":"Research & Development",
+    "companyName":"n/a",
+    "companyLocation":"Kajang, Selangor",
+    "duration":"(2018)",
+    "details":[
+      "Studied ReactJS, Redux(saga) &amp; D3 for personal project, demo at <a href='https://demo.piengeng.com' target='_blank' rel='noopener noreferrer'>demo.piengeng.com</a>.",
+      "Initial plan is to use ReactJS knowledge to segue into React Native project."
+    ]
+  },
+  {
+    "title":"Senior Software Engineer",
+    "companyName":"Nettium Sdn. Bhd.",
+    "companyLocation":"Kuala Lumpur",
+    "duration":"(2018)",
+    "details":[
+      "A transfer from Eagle Eye Technologies Sdn. Bhd. due to closure &amp; cost saving.",
+      "Continuation from previous company, same job, different office.",
+      "Bet logs core system additional platforms: TopTrendGaming, DreamTech &amp; QTech.",
+      "Bet logs API &amp; automation script development with Laravel &amp; CodeIgniter."
+    ]
+  },
+  {
+    "title":"Senior PHP Developer",
+    "companyName":"Eagle Eye Technologies Sdn. Bhd.",
+    "companyLocation":"Kuala Lumpur",
+    "duration":"(2016 - 2017)",
+    "details":[
+      "Full stack development, initially involved in front end, back end at later stage.",
+      "Extensive use of CodeIgniter framework, MySQL as database and Redis for caching.",
+      "Gaming platforms integrations: PlayTech, Microgaming, Gold Deluxe and Li-Bianc.",
+      "Responsible for automated retrieval and transformation of terabytes sized bet logs to be later used for business intelligence purpose.",
+      "Auto-withdrawal, code migration (complex due to system redesign yet keeping legacy C++ application) and logic verifications.",
+      "Bug fixes and general improvements on legacy codes, front-end and back office (HTML, JavaScript, views and controllers) and back-end (api service layer).",
+      "The 'go-to person' for complex and untestable (Non-UI) tasks.<!-- due to limited experienced programmers in the office -->",
+      "Writing MySQL triggers, functions and procedures.",
+      "Study on microservice api development in Python using Flask microframework and SQLAlchemy.",
+      "Tools used: Jira, Bitbucket, bash scripting, crontab, JMeter (load testing), Acunetix (paid web-based pen testing), Wireshark (network analyzer) and VirtualBox."
+    ]
+  },
+  {
+    "title":"Self Studies",
+    "companyName":"n/a",
+    "companyLocation":"Kajang, Selangor",
+    "duration":"(2014 - 2016)",
+    "details":[
+      "Focused on various web technologies.",
+      "Node.js/npm web development with Gulp/Grunt build system.",
+      "Administration and development in Linux, Apache, MySQL/PostgreSQL and PHP.",
+      "Laravel (MVC and TDD) development.",
+      "HTML5 and CSS3 (Bootstrapâ€™s SASS).",
+      "Exim4 email server configuration."
+    ]
+  },
+  {
+    "title":"Product Engineer",
+    "companyName":"STMicroelectronics Sdn. Bhd.",
+    "companyLocation":"Muar, Johor",
+    "duration":"(2010 - 2013)",
+    "details":[
+      "Integrated circuits test program development and maintenance, specialized in Teradyne FLEX system.",
+      "Carry out quality assurance on new test program.",
+      "Perform yield improvement activities."
+    ]
+  },
+  {
+    "title":"Consultant",
+    "companyName":"Provectus Sdn. Bhd.",
+    "companyLocation":"Petaling Jaya, Selangor",
+    "duration":"(2009 - 2010)",
+    "details":[
+      "Perform change management for Malaysia Airports Holding Berhad.",
+      "Lead meetings and maintain relationships with various stakeholders in the project."
+    ]
+  },
+  {
+    "title":"Product Engineer",
+    "companyName":"Huawei Technologies (M) Sdn. Bhd.",
+    "companyLocation":"Kuala Lumpur",
+    "duration":"(2008 - 2009)",
+    "details":[
+      "GSM/UMTS/NGN Core Network technical sales support across Southeast Asia region.",
+      "Designed core network and written proposals for projects over USD10 million. Customers include CAMSHIN (Cambodia) &amp; GLOBE (Philippines).",
+      "Prepared documents for contract (e.g. Quotation/Purchase Order).",
+      "Presentation to customers in response to RFP/RFQ."
+    ]
+  },
+  {
+    "title":"Design Engineer",
+    "companyName":"Sony EMCS (M) Sdn. Bhd.",
+    "companyLocation":"Perai, Pulau Pinang",
+    "duration":"(2005 - 2008)",
+    "details":[
+      "Supervised production floor to meet quality requirements.",
+      "Coordinated with team members to meet specific timeline during product design phases.",
+      "Cooperated with external departments to solve problems.",
+      "Maintained departmental documents and website.",
+      "Performed value analysis and carried out cost down activities.",
+      "Nurtured in production and manufacturing environment (Poka-yoke approach, Six Sigma, FMEA, TQM, Zero Defect, Kaizen culture, inventory and quality audits)."
+    ]
+  }
+]
+}
